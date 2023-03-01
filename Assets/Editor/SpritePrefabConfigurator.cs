de91095a8b515ae1c397fb94154d136bb3db6239
William Ullholm <90192382+WilUll@users.noreply.github.com>
2023-03-01 16:42:09 +0100

    Updated Editor Tool

--- Assets/Editor/SpritePrefabConfigurator.cs
+++ Assets/Editor/SpritePrefabConfigurator.cs
@@ -7,6 +7,10 @@ using UnityEngine.Rendering;
 
 public class SpritePrefabConfigurator : EditorWindow
 {
+    private List<GameObject> listOfChildren;
+    
+    private const string MAT_PATH = "Assets/Shaders/SpriteShadow.mat";
+
     [MenuItem("Window/Sprite Prefab Configurator")]
     public static void ShowWindow()
     {
@@ -16,19 +20,74 @@ public class SpritePrefabConfigurator : EditorWindow
     private void OnGUI()
     {
         GUILayout.Label("Sprite Prefab Configurator", EditorStyles.boldLabel);
-        if (GUILayout.Button("Configure"))
+        if (GUILayout.Button("Fix Sprite Layers"))
         {
-            Configure();
+            AddSortingGroup();
+        }
+        
+        if (GUILayout.Button("Fix Sprite Rotation"))
+        {
+            SpriteRotation();
+        }
+        
+        if (GUILayout.Button("Add Shadows"))
+        {
+            AddShadows();
         }
         
     }
 
-    private void Configure()
+    private void AddSortingGroup()
     {
         foreach (GameObject gameObjects in Selection.gameObjects)
         {
             gameObjects.AddComponent<SortingGroup>();
+        }
+    }
+
+    private void SpriteRotation()
+    {
+        foreach (GameObject gameObjects in Selection.gameObjects)
+        {
             gameObjects.transform.rotation = Quaternion.Euler(45, 0, 0);
+        }
+    }
+    
+    private void AddShadows()
+    {
+        foreach (GameObject gameObjects in Selection.gameObjects)
+        {
+            GetChildRecursive(gameObjects);
+            foreach (GameObject child in listOfChildren)
+            {
+                var sr = child.GetComponent<SpriteRenderer>();
+                sr.receiveShadows = true;
+                sr.shadowCastingMode = ShadowCastingMode.On;
+                
+                Material newMat = (Material) AssetDatabase.LoadAssetAtPath(MAT_PATH, typeof(Material));
+                if(newMat != null)
+                    Debug.Log("Asset loaded");
+                else
+                    Debug.Log("cant find asset");
+             
+                sr.material = newMat;
+            }
+            listOfChildren.Clear();
+        }
+    }
+
+    private void GetChildRecursive(GameObject obj){
+        if (null == obj)
+            return;
+        
+        listOfChildren.Add(obj);
+
+        foreach (Transform child in obj.transform){
+            if (null == child)
+                continue;
+            //child.gameobject contains the current child you can do whatever you want like add it to an array
+            listOfChildren.Add(child.gameObject);
+            GetChildRecursive(child.gameObject);
         }
     }
 }
